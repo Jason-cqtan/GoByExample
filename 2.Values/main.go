b4192b3ec1e12b013668190fb08c2a2470a81b24
谭佳成 <uniqueacheng@gmail.com>
2022-03-25 19:21:07 +0800

    16

---
+++ 2.Values/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + " land")
+
+	fmt.Printf("1 + 1= %d\n", 1+1)
+
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false)
+
+	fmt.Println(true || false)
+
+	fmt.Println(!true)
+}
